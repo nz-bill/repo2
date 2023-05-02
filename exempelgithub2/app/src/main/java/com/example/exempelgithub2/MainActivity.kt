2d1fce8d1efd91825faa808999c24255756f05ac
nz-bill <bill.palmestedt@zocom.se>
2023-05-02 14:30:36 +0200

    extracted advanced function setX

--- exempelgithub2/app/src/main/java/com/example/exempelgithub2/MainActivity.kt
+++ exempelgithub2/app/src/main/java/com/example/exempelgithub2/MainActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var x : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val x = 45
+       setX(45)
+    }
+    
+    fun setX(x: Int){
+        this.x = x
 
         println(x)
     }
